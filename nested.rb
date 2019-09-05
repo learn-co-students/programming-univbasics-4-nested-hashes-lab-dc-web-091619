8e6614a499b1310041e0c0ceab5b79224b87aad9
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-05 19:01:52 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -14,10 +14,9 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
-        programmer_hash[:grace_hopper => ]
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -38,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,17 +56,16 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+ 		    :known_for => "Ruby",
+ 		    :languages => ["LISP", "C"]
+ 		  },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
